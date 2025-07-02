e12f89858b25eb205a3db8283f7d6727e97834e3
simrok <162396191+simrok@users.noreply.github.com>
2025-07-02 21:42:30 +0900

    Title: 달에서 무게 구하기, Time: 101ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250702/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weightC=13;
+        double weightM=0.165;
+        double count = weightC * weightM;
+        System.out.printf("%d * %f = %.6f", weightC, weightM, count);
+    }
+}
