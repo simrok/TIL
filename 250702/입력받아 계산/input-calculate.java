271a30b4767e8ed8357e1f2bef80b5e3d2a4b50b
simrok <162396191+simrok@users.noreply.github.com>
2025-07-02 22:38:59 +0900

    Title: 입력받아 계산, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250702/입력받아 계산/input-calculate.java
+++ 250702/입력받아 계산/input-calculate.java
@@ -1,10 +1,11 @@
-import java.util.Scannner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = nextLine();
+        String input = scanner.nextLine();
         int a = Integer.parseInt(input);
         System.out.println(a + 2);
+        scanner.close();
     }
 }
