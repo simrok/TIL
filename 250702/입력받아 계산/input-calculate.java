daab226b2dd8d4f745007629438a2546feb15739
simrok <162396191+simrok@users.noreply.github.com>
2025-07-02 22:37:39 +0900

    Title: 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250702/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scannner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = nextLine();
+        int a = Integer.parseInt(input);
+        System.out.println(a + 2);
+    }
+}
