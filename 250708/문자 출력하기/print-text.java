646dabd544213116e6e6f152900176d9fd15dfcb
simrok <162396191+simrok@users.noreply.github.com>
2025-07-08 15:50:09 +0900

    Title: 문자 출력하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250708/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        input = scanner.nextLine();
+
+        for(int i=0;i<=7;i++) {
+            System.out.print(input);
+        }
+    }
+}
