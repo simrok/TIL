bb18d97b13b74ab760025d5259d3157fa17a4310
simrok <162396191+simrok@users.noreply.github.com>
2025-07-08 15:53:08 +0900

    Title: 문자열 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250708/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        input = scanner.nextInt();
+        for (int i = 0; i<input;i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
