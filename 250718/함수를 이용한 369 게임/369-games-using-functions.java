f86d61d1856f66fff13a3d888a0617f0981f39bc
simrok <162396191+simrok@users.noreply.github.com>
2025-07-18 17:45:12 +0900

    Title: 함수를 이용한 369 게임, Time: 207ms, Memory: 19MB, Status: Passed - Codetree

--- 250718/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250718/함수를 이용한 369 게임/369-games-using-functions.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;
+
 public class Main {
-    public static int func1(int a, int b) {
-        int count=0;
-        for(int i=a;i<=b;i++) {
-            if(i/10==3 || i/10==6 || i/10==9 || i%10==3 || i%10==6 || i%10==9 || i%3==0) {
-                count += 1;
+
+    // 조건을 만족하는지 확인하는 함수
+    public static boolean check(int n) {
+        if (n % 3 == 0) {
+            return true;
+        }
+
+        String str = String.valueOf(n);
+        return str.contains("3") || str.contains("6") || str.contains("9");
+    }
+
+    // A부터 B까지 검사하며 check() 함수 호출
+    public static int count(int a, int b) {
+        int result = 0;
+        for (int i = a; i <= b; i++) {
+            if (check(i)) {
+                result++;
             }
         }
-        return count;
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        // Please write your code here.
-        System.out.println(func1(A, B));
-    }   
+
+        System.out.println(count(A, B));
+    }
 }
